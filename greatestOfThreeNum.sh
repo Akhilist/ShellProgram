3236c7a6b5799b189ef54f81b164df9c11b41995
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-04-17 17:56:36 +0530

    Greates number among three num  program completed

--- greatestOfThreeNum.sh
+++ greatestOfThreeNum.sh
@@ -1,10 +1,4 @@
-echo "Enter Num1:"
-read num1
-echo "Enter Num2:"
-read num2
-echo "Enter Num3:"
-read num3
-
+read -p "Enter Three number :" num1 num2 num3 ;
 if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
 then
     echo $num1
