078dbe622b615dc2414ea4964ecca8f78b54dfd9
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-04-17 17:37:54 +0530

    Area of the circle program completed

--- area_of_circle_MR.sh
+++ area_of_circle_MR.sh
@@ -1,4 +1,3 @@
-echo "Enter the radius : "
-read r
-echo "Area of the Circle is"
-echo "3.14 * $r * $r" | bc
+read -p "Enter radius: " radius ;
+echo "Area of the circle is "
+echo 3.14*$radius*$radius | bc;
