1aea5b5537379f82e9badc2dce7141c37f90c5a9
Mubashir Angathil <mubashirangathil5142@gmail.com>
2022-04-17 17:18:05 +0530

    Armostrong number program completed

--- Armstrong.sh
+++ Armstrong.sh
@@ -1,25 +1,21 @@
-echo "Enter the number"
-read n
-function ams
-{
-t=$n
-s=0
-b=0
-c=10
-while [ $n -gt $b ]
-do
-r=$((n % c))
-i=$((r * r * r))
-s=$((s + i))
-n=$((n / c))
-done
-#echo $s
-if [ $s == $t ]
-then
-echo "Armstrong number"
-else
-echo "Not an Armstrong number"
-fi
+read -p "Enter the number: " n
+function ams {
+    t=$n
+    s=0
+    b=0
+    c=10
+    while [ $n -gt $b ]; do
+        r=$((n % c))
+        i=$((r * r * r))
+        s=$((s + i))
+        n=$((n / c))
+    done
+    #echo $s
+    if [ $s == $t ]; then
+        echo "an Armstrong number"
+    else
+        echo "Not an Armstrong number"
+    fi
 }
-result=`ams`
-echo $t" is $result"
+result=$(ams)
+echo $t"$n is $result"
